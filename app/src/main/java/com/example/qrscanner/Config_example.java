4654656889a0dc9376684ce7408399df98d442b4
Samiya Akhtar <samiyaakhtar7@gmail.com>
2015-06-23 01:43:48 -0400

    Updated Config_example to reflect the code cleanup

--- app/src/main/java/com/example/qrscanner/Config_example.java
+++ app/src/main/java/com/example/qrscanner/Config_example.java
@@ -2,10 +2,6 @@ package com.example.qrscanner;
 
 public class Config_example {
 
-	public static String CLIENT_ID = "<your_client_id>";
-	public static String CLIENT_SECRET = "<client_secret_id>";
-	public static String SPREADSHEET_URL = "<spreadsheet_url>";
-	public static String SPREADSHEET_PUBLIC_URL = "<published_spreadsheet_url>";
     public static String PARSE_APP_ID = "Your app id";
     public static String PARSE_CLIENT_ID = "Your client id";
 }
